14e743c4ae73dbcb9d73149fd61ac8929f611866
garfieldvans <iyanhalo@gmail.com>
2024-05-16 21:28:19 +0700

    [project-init]

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import SignUp from "./pages/page";
+
+export default function Home() {
+  return (
+    <div className="">
+      <SignUp/>
+    </div>
+  );
+}
---
+++ app/pages/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1 className='font-bold text-center text-4xl'>Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard
